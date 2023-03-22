592682fe03a61bd4b6ae7ee262256d854367afab
addielmoldes <addielpunk99@gmail.com>
2023-03-22 02:46:38 -0400

    gridview structure

---
+++ lib/content/content.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Content extends StatelessWidget {
+  const Content({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contenido 1'),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:tce/info.dart';
+import 'package:tce/content/content.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    title: 'TCE',
-    home: Home(),
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TCE',
+      home: const Home(),
+      theme: ThemeData(
+        useMaterial3: false,
+        primarySwatch: Colors.green,
+        // colorScheme: const ColorScheme.dark(),
+      ),
+    );
+  }
 }
 
 class Home extends StatelessWidget {
@@ -17,75 +38,120 @@ class Home extends StatelessWidget {
         title: const Text('Contenido'),
       ),
       drawer: Drawer(
-          child: ListView(children: const <Widget>[
-        DrawerHeader(
-            decoration: BoxDecoration(
-                color: Colors.blue,
-                image: DecorationImage(
-                    image: AssetImage('assets/images/test.jpg'),
-                    fit: BoxFit.cover)),
-            child: null),
-        ListTile(
-          leading: Icon(Icons.menu_book),
-          title: Text('Contenido'),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/test.jpg'),
+                        fit: BoxFit.cover)),
+                child: null),
+            const ListTile(
+              leading: Icon(Icons.menu_book),
+              title: Text('Contenido'),
+            ),
+            const ListTile(
+              leading: Icon(Icons.fact_check),
+              title: Text('Evaluar'),
+            ),
+            const Divider(),
+            ListTile(
+              leading: const Icon(Icons.info),
+              title: const Text('Información'),
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const Info(),
+                ),
+              ),
+            ),
+          ],
         ),
-        ListTile(
-          leading: Icon(Icons.fact_check),
-          title: Text('Evaluar'),
-        ),
-        Divider(),
-        ListTile(
-          leading: Icon(Icons.info),
-          title: Text('Info'),
-        )
-      ])),
-      body: ListView(
+      ),
+      body: GridView.count(
+        physics: const BouncingScrollPhysics(),
+        crossAxisCount: 2,
+        mainAxisSpacing: 3.0,
+        crossAxisSpacing: 3.0,
         children: <Widget>[
           customCard(
-              'assets/images/test.jpg', 'Contenido 1', 'Explicacion breve'),
+            'assets/images/consc.JPG',
+            'Nivel de conciencia',
+            context,
+            const Content(),
+          ),
           customCard(
-              'assets/images/test.jpg', 'Contenido 2', 'Explicacion breve'),
+            'assets/images/pupil.JPG',
+            'Pupilas',
+            context,
+            const Content(),
+          ),
           customCard(
-              'assets/images/test.jpg', 'Contenido 3', 'Explicacion breve')
+            'assets/images/test.jpg',
+            'Signos irritativos',
+            context,
+            const Content(),
+          ),
+          customCard(
+            'assets/images/test.jpg',
+            'Focalidad motora',
+            context,
+            const Content(),
+          ),
+          customCard(
+            'assets/images/test.jpg',
+            'Otros',
+            context,
+            const Content(),
+          ),
         ],
       ),
     );
   }
 }
 
-Widget customCard(String imgPath, text, subText) {
+Widget customCard(imgPath, title, context, route) {
   return Card(
-    child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        ListTile(
-          leading: Image.asset(imgPath),
-          title: Text(text),
-          subtitle: Text(subText),
+    elevation: 2,
+    child: GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => route),
+      ),
+      child: GridTile(
+        footer: Container(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(5),
+              bottomRight: Radius.circular(5),
+            ),
+            color: Color.fromARGB(175, 0, 0, 0),
+          ),
+          padding: const EdgeInsets.all(4.0),
+          child: Text(
+            title,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 14.0,
+              // fontWeight: FontWeight.bold,
+              letterSpacing: 1.0,
+            ),
+          ),
         ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: const <Widget>[
-            TextButton(onPressed: null, child: Text('LEER MÁS'))
-          ],
-        )
-      ],
+        child: Container(
+          padding: EdgeInsets.zero,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(imgPath),
+              fit: BoxFit.cover,
+            ),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(5),
+            ),
+          ),
+        ),
+      ),
     ),
-  );
-}
-
-Widget customDrawerText() {
-  return Column(
-    mainAxisAlignment: MainAxisAlignment.end,
-    children: <Widget>[
-      Row(
-        children: const <Widget>[
-          Text(
-            'Menu',
-            textAlign: TextAlign.start,
-          )
-        ],
-      )
-    ],
   );
 }
